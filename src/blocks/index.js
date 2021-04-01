d3c345b8dea6ea52859a5ff094f026d6c21e33cb
retrofox <rdsuarez@gmail.com>
2021-04-01 16:36:58 -0300

    bloks: re-organize files structure

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -1,24 +1,4 @@
-
-/**
- * WordPress dependencies
- */
-import { registerBlockType } from '@wordpress/blocks';
-
 /**
  * Internal dependencies
  */
-import { MediaTheaterIcon as icon } from '../icons';
-
-/**
- * Internal dependencies
- */
-import './style.scss';
-import Edit from './edit';
-import save from './save';
-
-registerBlockType( 'media-center/media-theater', {
-	apiVersion: 2,
-	edit: Edit,
-	save,
-	icon,
-} );
+import './media-theater';
---
+++ src/blocks/media-theater/index.js
@@ -0,0 +1,24 @@
+
+/**
+ * WordPress dependencies
+ */
+import { registerBlockType } from '@wordpress/blocks';
+
+/**
+ * Internal dependencies
+ */
+import { MediaTheaterIcon as icon } from '../../icons';
+
+/**
+ * Internal dependencies
+ */
+import './style.scss';
+import Edit from './edit';
+import save from './save';
+
+registerBlockType( 'media-center/media-theater', {
+	apiVersion: 2,
+	edit: Edit,
+	save,
+	icon,
+} );
