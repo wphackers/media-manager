088fde1f8d07a70eae1f4ee8efcf213b42a776ad
retrofox <rdsuarez@gmail.com>
2021-04-01 16:07:57 -0300

    media-theater: clean. add custom icon.

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -1,43 +1,24 @@
+
 /**
- * Registers a new block provided a unique name and an object defining its behavior.
- *
- * @see https://developer.wordpress.org/block-editor/developers/block-api/#registering-a-block
+ * WordPress dependencies
  */
 import { registerBlockType } from '@wordpress/blocks';
 
 /**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * All files containing `style` keyword are bundled together. The code used
- * gets applied both to the front of your site and to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ * Internal dependencies
  */
-import './style.scss';
+import { MediaTheaterIcon as icon } from '../icons';
 
 /**
  * Internal dependencies
  */
+import './style.scss';
 import Edit from './edit';
 import save from './save';
 
-/**
- * Every block starts by registering a new block type definition.
- *
- * @see https://developer.wordpress.org/block-editor/developers/block-api/#registering-a-block
- */
-registerBlockType( 'media-center/media-center', {
-	/**
-	 * @see https://make.wordpress.org/core/2020/11/18/block-api-version-2/
-	 */
+registerBlockType( 'media-center/media-theater', {
 	apiVersion: 2,
-
-	/**
-	 * @see ./edit.js
-	 */
 	edit: Edit,
-
-	/**
-	 * @see ./save.js
-	 */
 	save,
+	icon,
 } );
