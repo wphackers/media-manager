8b107e9bbe16c84ecceb49a62f39a81dbe34a97d
retrofox <rdsuarez@gmail.com>
2021-04-16 13:12:01 -0300

    utils: quick time helpers bug

--- src/lib/time-utils.js
+++ src/lib/time-utils.js
@@ -1,7 +1,12 @@
 /* global mejs */
 
 export function convertSecondsToTimeCode( seconds ) {
+	if ( ! seconds ) {
+		return '00:00:00';
+	}
 	return new Date( seconds * 1000 ).toISOString().substr( 11, 8 );
 }
 
-export const convertTimeCodeToSeconds = mejs.Utils.timeCodeToSeconds;
+export const convertTimeCodeToSeconds = typeof mejs !== 'undefined'
+	? mejs.Utils.timeCodeToSeconds
+	: () => {};
