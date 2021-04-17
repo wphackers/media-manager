131cfe55d4e93dce4e1dd0633bc1fc03b1e1eb76
retrofox <rdsuarez@gmail.com>
2021-04-17 10:30:45 -0300

    extending: register media blocks in frontend

--- src/extending/media-blocks/view.js
+++ src/extending/media-blocks/view.js
@@ -2,12 +2,30 @@
  * External dependencies
  */
 import domReady from '@wordpress/dom-ready';
-import { select } from '@wordpress/data';
+import { dispatch } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
+import { STORE_ID, STATE_PAUSED } from '../../store/constants';
+import { getBlockSourceProps } from '../utils';
 
 domReady( function() {
-	console.log( 'ready' );
+	// Register media element in the store.
+
+	const mediaElements = document.querySelectorAll( '.media-center-media-source' );
+	if ( mediaElements.length ) {
+		mediaElements.forEach( function( media ) {			
+			const { mediaSourceId, blockName } = media.dataset;
+			const { domTypeName } = getBlockSourceProps( blockName );
+			const query = `.media-center-media-source[data-media-source-id="${ mediaSourceId }"] ${ domTypeName }`;
+
+			dispatch( STORE_ID ).registerMediaSource( mediaSourceId, {
+				// source: mediaSource,
+				elementType: domTypeName,
+				state: STATE_PAUSED,
+				querySelector: query,
+			} );
+		} );
+	}
 } );
