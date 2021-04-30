d8f77e2896b682f403a971030afc2e6304581be4
retrofox <rdsuarez@gmail.com>
2021-04-30 13:59:47 -0300

    media-selector: refactoring code - add panel cmp

--- src/components/media-selector/index.js
+++ src/components/media-selector/index.js
@@ -6,7 +6,7 @@ import Gridicon from 'gridicons';
 /**
  * WordPress dependencies
  */
-import { useRef, useEffect, useState } from '@wordpress/element';
+import { useRef, useEffect, useState, select } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { PanelBody } from '@wordpress/components';
 
@@ -20,12 +20,23 @@ import { PlayPauseButton } from '../media-player';
 export function MediaItem( {
 	elementType: type,
 	source,
-	onItemSelect = () => {},
 	id,
-	dissablePlayInHover = false,
+	showTypeLabel,
+	onPlayToggle = () => {},
+	onItemSelect = () => {},
 	onReady = () => {},
+	onRemove = () => {},
 } ) {
 	const itemReference = useRef();
+	const [ isPlaying, setIsPlaying ] = useState( false );
+
+	function onPlayHandler() {
+		setIsPlaying( true );
+	}
+
+	function onPauseHandler() {
+		setIsPlaying( false );
+	}
 
 	useEffect( () => {
 		if ( ! itemReference?.current ) {
@@ -38,21 +49,46 @@ export function MediaItem( {
 
 		// Insert player into the DOM.
 		itemReference.current.appendChild( mediaElement );
-		onReady( mediaElement );
-	}, [] );
+		onReady( mediaElement, id );
+
+		mediaElement.addEventListener( 'play', onPlayHandler );
+		mediaElement.addEventListener( 'pause', onPauseHandler );
+
+		return function() {
+			onRemove( mediaElement, id );
+			mediaElement.removeEventListener( 'play', onPlayHandler );
+			mediaElement.removeEventListener( 'pause', onPauseHandler );
+		}
+	}, [ itemReference ] );
 
 	return (
 		<div
 			className="media__item-wrapper"
-			onMouseEnter={ ( { target } ) =>
-				! dissablePlayInHover && target.querySelector( type )?.play()
-			}
-			onMouseLeave={ ( { target } ) =>
-				! dissablePlayInHover && target.querySelector( type )?.pause()
-			}
 			onClick={ () => onItemSelect( id ) }
 		>
 			<div className="media__item" ref={ itemReference } />
+
+			<div className="media__item-metadata">
+				<Gridicon
+					className={ `media__item-icon is-${ type }` }
+					icon={ type }
+					size={ 36 }
+				/>
+
+				{ showTypeLabel && (
+					<strong> { type }</strong>
+				) }
+
+				<PlayPauseButton
+					className="media__item-button"
+					isPaused={ ! isPlaying }
+					onClick={ ( event ) => {
+						event.stopPropagation();
+						onPlayToggle( itemReference.current?.querySelector( type ), id );
+					} }
+					isSmall
+				/>
+			</div>
 		</div>
 	);
 }
@@ -65,42 +101,27 @@ export function MediaItemPanelBody( {
 		return null;
 	}
 
-	const mediaRef = useRef();
-	const [ isPaused, setIsPaused ] = useState( true );
-
 	return (
 		<PanelBody className="media-source-panel" title={ title }>
 			<p>{ __( 'Media source connected to the theater', 'media-center' ) }</p>
 
 			<MediaItem
 				{ ...source }
-				dissablePlayInHover={ true }
-				onReady={ function ( mediaElement ) {
-					mediaRef.current = mediaElement;
+				showTypeLabel={ true }
+				onPlayToggle={ ( element ) => {
+					if ( ! element ) {
+						return;
+					}
+
+					if ( element.paused ) {
+						return element.play();
+					}
+
+					element.pause();
 				} }
 			/>
 
 			<ul>
-				<li className="media-type">
-					<Gridicon
-						className={ `media__item-icon is-${ source.elementType }` }
-						icon={ source.elementType }
-						size={ 36 }
-					/>
-					<strong> { source.elementType }</strong>
-					<PlayPauseButton
-						isPaused={ isPaused }
-						onClick={ function() {
-							if ( mediaRef.current?.paused ) {
-								setIsPaused( false );
-								return mediaRef.current.play();
-							}
-
-							setIsPaused( true );
-							mediaRef.current.pause();
-						} }
-					/>
-				</li>
 				<li>
 					{ __( 'Filename:', 'media-center' ) }
 					<strong> { source.source }</strong>
@@ -119,6 +140,8 @@ export default function MediaSelector( { media, onMediaSelect } ) {
 		return null;
 	}
 
+	const [ mediaPlayingElementRef, setMediaPlayingElementRef ] = useState();
+
 	return (
 		<div className="media-selector">
 			<h4>
@@ -128,13 +151,28 @@ export default function MediaSelector( { media, onMediaSelect } ) {
 				) }
 			</h4>
 			<ul>
-				{ media.map( function ( { elementType, source, id } ) {
+				{ media.map( function ( { id, ...other } ) {
 					return (
 						<li key={ `item-${ id }` }>
 							<MediaItem
 								id={ id }
-								type={ elementType }
-								source={ source }
+								{ ...other }
+								isPaused={ mediaPlayingElementRef?.paused }
+								onPlayToggle={ ( element, mediaId ) => {
+									// Seek previous media element by media Id.
+									// If so, stop it.
+									if ( mediaPlayingElementRef && mediaPlayingElementRef !== element) {
+										mediaPlayingElementRef?.pause();
+									};
+
+									if ( element.paused ) {
+										setMediaPlayingElementRef( element );
+										return element.play();
+									}
+				
+									setMediaPlayingElementRef( null );
+									element.pause();
+								} }
 								onItemSelect={ onMediaSelect }
 							/>
 						</li>
