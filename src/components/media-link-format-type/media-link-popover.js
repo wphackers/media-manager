bbdfd8e2479407907bfa09065ac941f83de74282
retrofox <rdsuarez@gmail.com>
2021-05-08 12:05:21 -0300

    move MediaPlayerControl to components

--- src/components/media-link-format-type/media-link-popover.js
+++ src/components/media-link-format-type/media-link-popover.js
@@ -1,98 +1,15 @@
 /**
- * External dependencies
- */
-import { debounce } from 'lodash';
-
-/**
  * WordPress dependencies
  */
-import { Popover, Button, RangeControl } from '@wordpress/components';
+import { Popover } from '@wordpress/components';
 import { useAnchorRef } from '@wordpress/rich-text';
 import { __ } from '@wordpress/i18n';
-import { useSelect, useDispatch } from '@wordpress/data';
-import { useState, useEffect } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
-import { convertSecondsToTimeCode, convertTimeCodeToSeconds } from '../../lib/time-utils';
 import { mediaLinkFormatButtonSettings } from './';
-import { STORE_ID, STATE_PAUSED, STATE_PLAYING } from '../../store/constants';
-import { PlayPauseButton, SkipForwardButton, JumpBackButton } from '../media-player';
-
-const debouncedOnChange = debounce( function ( time, onChange ) {
-	onChange( time );
-}, 250 );
-
-export function MediaPlayerControl( {
-	sourceId,
-	time,
-	onChange = () => {},
-} ) {
-	const [ rangeTime, setRangeTime ] = useState( time );
-	useEffect( () => setRangeTime( time ), [ time ] );
-
-	const { mediaPlayingState, mediaDuration } = useSelect(
-		( select ) => ( {
-			mediaPlayingState: select( STORE_ID ).getMediaPlayerState(
-				sourceId
-			),
-			mediaDuration: select( STORE_ID ).getMediaSourceDuration( sourceId ),
-		} ),
-		[]
-	);
-
-	const { toggleMediaSource, setMediaSourceCurrentTime } = useDispatch(
-		STORE_ID
-	);
-
-	const isPaused = mediaPlayingState === STATE_PAUSED;
-
-	function toggleInTime() {
-		if ( mediaPlayingState !== STATE_PLAYING ) {
-			setMediaSourceCurrentTime( sourceId, time );
-		}
-		toggleMediaSource( sourceId );
-	}
-
-	function onChangeTimeHandler( time ) {
-		setRangeTime( time );
-		debouncedOnChange( time, onChange );
-	}
-
-	const currentTimeFormatted = convertSecondsToTimeCode( rangeTime );
-
-	return (
-		<div className="media-player-control">
-			<JumpBackButton
-				onClick={ () => onChangeTimeHandler( Math.max( 0, time - 5 ) ) }
-			/>
-
-			<PlayPauseButton
-				isPaused={ isPaused }
-				onClick={ () => toggleInTime() }
-			/>
-
-			<SkipForwardButton
-				onClick={ () => onChangeTimeHandler( Math.min( mediaDuration, time + 5 ) ) }
-			/>
-
-			<div className="media-player-control__display">
-				{ currentTimeFormatted }
-			</div>
-
-			<RangeControl
-				disabled={ typeof mediaDuration === 'undefined' }
-				value={ rangeTime }
-				min={ 0 }
-				max={ mediaDuration }
-				onChange={ onChangeTimeHandler }
-				withInputField={ false }
-				showTooltip={ false }
-			/>
-		</div>
-	);
-}
+import { MediaPlayerControl } from '../media-player';
 
 export default function MediaLinkPopover( {
 	value,
--- src/components/media-player/index.js
+++ src/components/media-player/index.js
@@ -1,9 +1,17 @@
 
 /**
+ * External dependencies
+ */
+ import { debounce } from 'lodash';
+ 
+ /**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
+import { useSelect, useDispatch } from '@wordpress/data';
+import { useState, useEffect } from '@wordpress/element';
+import { RangeControl } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -14,6 +22,8 @@ import {
 	PlayerPlayIcon,
 	PlayerPauseIcon,
 } from '../../icons';
+import { convertSecondsToTimeCode } from '../../lib/time-utils';
+import { STORE_ID, STATE_PAUSED, STATE_PLAYING } from '../../store/constants';
 
 export function JumpBackButton( { onClick, ...other } ) {
 	return (
@@ -49,5 +59,79 @@ export function SkipForwardButton( { onClick } ) {
 			onClick={ onClick }
 			label={ __( 'Skip forward', 'media-manager' ) }
 		/>
+	);
+}
+
+const debouncedOnChange = debounce( function ( time, onChange ) {
+	onChange( time );
+}, 250 );
+
+export function MediaPlayerControl( {
+	sourceId,
+	time,
+	onChange = () => {},
+} ) {
+	const [ rangeTime, setRangeTime ] = useState( time );
+	useEffect( () => setRangeTime( time ), [ time ] );
+
+	const { mediaPlayingState, mediaDuration } = useSelect(
+		( select ) => ( {
+			mediaPlayingState: select( STORE_ID ).getMediaPlayerState(
+				sourceId
+			),
+			mediaDuration: select( STORE_ID ).getMediaSourceDuration( sourceId ),
+		} ),
+		[]
+	);
+
+	const { toggleMediaSource, setMediaSourceCurrentTime } = useDispatch(
+		STORE_ID
+	);
+
+	const isPaused = mediaPlayingState === STATE_PAUSED;
+
+	function toggleInTime() {
+		if ( mediaPlayingState !== STATE_PLAYING ) {
+			setMediaSourceCurrentTime( sourceId, time );
+		}
+		toggleMediaSource( sourceId );
+	}
+
+	function onChangeTimeHandler( time ) {
+		setRangeTime( time );
+		debouncedOnChange( time, onChange );
+	}
+
+	const currentTimeFormatted = convertSecondsToTimeCode( rangeTime );
+
+	return (
+		<div className="media-player-control">
+			<JumpBackButton
+				onClick={ () => onChangeTimeHandler( Math.max( 0, time - 5 ) ) }
+			/>
+
+			<PlayPauseButton
+				isPaused={ isPaused }
+				onClick={ () => toggleInTime() }
+			/>
+
+			<SkipForwardButton
+				onClick={ () => onChangeTimeHandler( Math.min( mediaDuration, time + 5 ) ) }
+			/>
+
+			<div className="media-player-control__display">
+				{ currentTimeFormatted }
+			</div>
+
+			<RangeControl
+				disabled={ typeof mediaDuration === 'undefined' }
+				value={ rangeTime }
+				min={ 0 }
+				max={ mediaDuration }
+				onChange={ onChangeTimeHandler }
+				withInputField={ false }
+				showTooltip={ false }
+			/>
+		</div>
 	);
 }
