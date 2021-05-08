ffb998e97f7902a6fb93540de3356f1b94c560fd
retrofox <rdsuarez@gmail.com>
2021-05-08 11:49:14 -0300

    utils: refact and improve time helpers

--- src/lib/time-utils.js
+++ src/lib/time-utils.js
@@ -1,4 +1,4 @@
-/* global mejs */
+const timecodeRegExp = new RegExp( /^(?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)$/ );
 
 export function convertSecondsToTimeCode( seconds ) {
 	if ( ! seconds ) {
@@ -7,6 +7,29 @@ export function convertSecondsToTimeCode( seconds ) {
 	return new Date( seconds * 1000 ).toISOString().substr( 11, 8 );
 }
 
-export const convertTimeCodeToSeconds = typeof mejs !== 'undefined'
-	? mejs.Utils.timeCodeToSeconds
-	: () => {};
+export const convertTimeCodeToSeconds = ( string ) => {
+	if ( ! isTimecode( string ) ) {
+		return;
+	}
+	let parts = string.match( timecodeRegExp );
+	if ( ! parts ) {
+		return;
+	}
+
+	// seconds.
+	let time = Number( parts[ 3 ] );
+
+	// minutes.
+	if ( parts[ 2 ] ) {
+		time += Number( parts[ 2 ] ) * 60;
+	}
+
+	// hours.
+	if ( parts[ 1 ] ) {
+		time += Number( parts[ 1 ] ) * 60 * 60;
+	}
+
+	return time;
+}
+
+export const isTimecode = ( value ) => timecodeRegExp.test( value );
