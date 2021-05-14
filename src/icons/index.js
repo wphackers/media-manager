faebb94f52bfd52ecf8c9ed3fb2336948ae276ff
retrofox <rdsuarez@gmail.com>
2021-05-14 19:06:06 -0300

    icons: add scale support to PlayerPlayIcon

--- src/icons/index.js
+++ src/icons/index.js
@@ -37,16 +37,29 @@ export const MediaLinkIcon = () => (
 	</SVG>
 );
 
-export const PlayerPlayIcon = () => (
-	<SVG
-		xmlns="https://www.w3.org/2000/svg"
-		viewBox="0 0 24 24"
-		width="24"
-		height="24"
-	>
-		<Path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z" />
-	</SVG>
-);
+export const PlayerPlayIcon = ( { scale = 1 } ) => {
+	const width = 24 * scale;
+	const height = 24 * scale;
+
+	const translate = ( scale - 1 ) * 12;
+	const inlineStyle = scale ==! 1
+		? null
+		: {
+			transform: `scale(${ scale }) translate( ${ translate }px, ${ translate }px )`,
+		};
+
+	return (
+		<SVG
+			style={ inlineStyle }
+			xmlns="https://www.w3.org/2000/svg"
+			viewBox={ `0 0 ${ width } ${ height }` }
+			width={ width }
+			height={ height }
+		>
+			<Path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z" />
+		</SVG>
+	);
+};
 
 export const PlayerPauseIcon = () => (
 	<SVG
