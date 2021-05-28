1b7f5de77660291cb8a5291a492e438e7027150b
retrofox <rdsuarez@gmail.com>
2021-05-28 19:55:54 -0300

    built

--- src/blocks/play-pause-button/edit.js
+++ src/blocks/play-pause-button/edit.js
@@ -14,6 +14,7 @@ import { useSelect, useDispatch } from '@wordpress/data';
  */
 import { STORE_ID, STATE_PAUSED } from '../../store/constants';
 import { PlayPauseButton } from '../../components/media-player';
+import './editor.scss';
 
 export default function PlayPauseEditBlock( { context, scale } ) {
 	const sourceId = context.mediaSourceId;
