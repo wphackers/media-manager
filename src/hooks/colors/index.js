fc8e30d507820341a4f3374cc815604d404003ff
retrofox <rdsuarez@gmail.com>
2021-06-06 09:07:24 -0300

    hooks: move to its own folder

--- src/hooks/colors/index.js
+++ src/hooks/colors/index.js
@@ -14,7 +14,7 @@ import { getBlockSupport } from '@wordpress/blocks';
 /**
  * External dependencies
  */
-import withPlayerButtonSettings from '../components/with-player-button-settings';
+import withPlayerButtonSettings from '../../components/with-player-button-settings';
 
 export const getMediaManagerColorSupport = ( settings ) => getBlockSupport( settings, 'media-manager/color' );
 export const getMediaManagerCustomClassNameSupport = ( settings ) => getBlockSupport( settings, 'media-manager/customClassName' );
