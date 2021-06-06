fdf7600af1d52d514830b3e6817d844f847e8315
retrofox <rdsuarez@gmail.com>
2021-06-06 10:21:15 -0300

    hook:media-selector: implement selector cmp

--- src/hooks/media-selector/index.js
+++ src/hooks/media-selector/index.js
@@ -1,13 +1,160 @@
 /**
+ * External dependencies
+ */
+import { values } from 'lodash';
+
+/**
  * WordPress dependencies
  */
 import { addFilter } from '@wordpress/hooks';
-import { getBlockSupport } from '@wordpress/blocks';
+import { getBlockSupport, createBlock } from '@wordpress/blocks';
+import { useState, useCallback, Fragment } from '@wordpress/element';
+import { useSelect, useDispatch } from '@wordpress/data';
+import { __ } from '@wordpress/i18n';
+import {
+	InspectorControls,
+	BlockControls,
+	store as blockEditorStore,
+} from '@wordpress/block-editor';
+import { Placeholder, Panel, Button, Toolbar } from '@wordpress/components';
 
 /**
- * External dependencies
+ * Internal dependencies
  */
+import { store as mediaManagerStore } from '../../store';
+import { MEDIA_NOT_DEFINED } from '../../store/constants';
+import { MediaCenterIcon } from '../../icons';
+import MediaSelector, { MediaItemPanelBody } from '../../components/media-selector/';
+
+const useInsertMediaBlock = () => {
+	const { insertBlock } = useDispatch( blockEditorStore );
+
+	return useCallback( async ( blockType = 'core/video', clientId, index = 0 ) => {
+		const mediaBlock = await createBlock( blockType );
+		await insertBlock( mediaBlock, index, clientId, false );
+	}, [ insertBlock ] );
+};
+
 export const SUPPORT_NAME = 'media-manager/media-selector';
+
+export function withMediaSelector( OriginalBlock ) {
+	return function ( props ) {
+		const { attributes, setAttributes, clientId } = props;
+		const [ isReplacing, setIsReplacing ] = useState( false );
+
+		const { sourceId } = attributes;
+		const { mediaSources } = useSelect( ( select ) => {
+			return {
+				mediaSources: select( mediaManagerStore ).getMediaSources(),
+			};
+		}, [] );
+	
+		const { mediaSource } = useSelect( ( select ) => {
+			return {
+				mediaSource: select( mediaManagerStore ).getMediaSourceById( sourceId )
+			};
+		}, [ sourceId ] );
+	
+		const setSourceId = ( sourceId ) => setAttributes( { sourceId } );
+		const insertMediaBlock = useInsertMediaBlock();
+	
+		function insertEmptyMediaBlock( type ) {
+			setSourceId( MEDIA_NOT_DEFINED );
+			setIsReplacing( false );
+			insertMediaBlock( type, clientId )
+		}
+		
+		if ( isReplacing || ! sourceId ) {
+			return (
+				<div>
+					<Placeholder
+						icon={ MediaCenterIcon }
+						label={ __( 'Media Center', 'media-manager' ) }
+						instructions={ __(
+							'Manage all media sources, comfortable, from your couch.',
+							'media-manager'
+						) }
+					>
+						<MediaSelector
+							media={ values( mediaSources ) }
+							onMediaSelect={ ( id ) => {
+								setSourceId( id );
+								setIsReplacing( false );
+							} }
+						/>
+	
+						<Button
+							isSecondary
+							label={ __( 'Continue adding a video block', 'media-manager' ) }
+							onClick={ () => insertEmptyMediaBlock( 'core/video' ) }
+						>
+							{ __( 'Start with Video block', 'media-manager' ) }
+						</Button>
+	
+						<Button
+							isSecondary
+							label={ __( 'Continue adding a audio block', 'media-manager' ) }
+							onClick={ () => insertEmptyMediaBlock( 'core/audio' ) }
+						>
+							{ __( 'Start with an Audio block', 'media-manager' ) }
+						</Button>
+	
+						{ ( isReplacing && sourceId !== MEDIA_NOT_DEFINED ) && (
+							<Button
+								isTertiary
+								label={ __( 'Cancel replacing media source', 'media-manager' ) }
+								onClick={ () => setIsReplacing( false ) }
+							>
+								{ __( 'Cancel replacement', 'media-manager' ) }
+							</Button>
+						) }
+	
+						{ ( ! isReplacing || sourceId === MEDIA_NOT_DEFINED ) && (
+							<Button
+								isTertiary
+								label={ __( 'Continue without media source', 'media-manager' ) }
+								onClick={ () => {
+									setIsReplacing( false );
+									setSourceId( MEDIA_NOT_DEFINED );
+								} }
+							>
+								{ __( 'Skip', 'media-manager' ) }
+							</Button>
+						) }
+					</Placeholder>
+				</div>
+			);
+		}
+
+		return (
+			<Fragment>
+				<BlockControls>
+					<Toolbar>
+						<Button onClick={ () => setIsReplacing( true ) }>
+							{
+								( sourceId !== MEDIA_NOT_DEFINED )
+									? __( 'Replace', 'media-manager' )
+									: __( 'Link', 'media-manager' )
+							}
+						</Button>
+					</Toolbar>
+				</BlockControls>
+
+				<InspectorControls>
+					<Panel>
+						<MediaItemPanelBody
+							source={ mediaSource }
+							mediaSourceId={ sourceId }
+							onReplace={ setIsReplacing }
+							onUnlink={ () => setSourceId( null ) }
+						/>
+					</Panel>
+				</InspectorControls>
+				<OriginalBlock { ...props } />
+			</Fragment>
+		);
+	}
+}
 
 function addMediaSelectorSupport( settings ) {
 	if ( ! getBlockSupport( settings, SUPPORT_NAME ) ) {
@@ -22,6 +169,7 @@ function addMediaSelectorSupport( settings ) {
 				type: 'string',
 			},
 		},
+		edit: withMediaSelector( settings.edit ),
 	}
 }
 
