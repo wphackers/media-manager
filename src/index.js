bbd80d2bb7fabb1142e30fdaf58c236237b6af50
retrofox <rdsuarez@gmail.com>
2021-07-07 18:56:18 -0300

    require and register media link format type

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 /**
- * External dependencies
+ * WordPress dependencies
  */
+import { registerFormatType } from '@wordpress/rich-text';
+import { mediaLink } from '@media-manager/format-library';
 
 /**
  * Internal dependencies
  */
 import './extending';
 import './block-library';
+
+registerFormatType( mediaLink.name, mediaLink.settings );
