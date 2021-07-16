f86b7e7b21496659754ac6d841c57a30791aa55a
retrofox <rdsuarez@gmail.com>
2021-07-16 15:03:10 -0300

    bin: finish first relase approach

--- bin/release/index.js
+++ bin/release/index.js
@@ -8,37 +8,37 @@ const shell = require( 'shelljs' );
  */
 const { version } = require( '../../package.json' );
 
-// if ( ! shell.which( 'git' ) ) {
-// 	shell.echo( 'Sorry, this script requires git' );
-// 	shell.exit( 1 );
-// }
+if ( ! shell.which( 'git' ) ) {
+	shell.echo( 'Sorry, this script requires git' );
+	shell.exit( 1 );
+}
 
-// if ( shell.exec( `git checkout -b v${ version } ` ).code !== 0 ) {
-// 	shell.echo( 'Error: Git commit failed' );
-// 	shell.exit( 1 );
-// }
-
-// if ( shell.exec( `mkdir -p release-${ version }` ).code !== 0 ) {
-// 	shell.echo( 'Error: Git commit failed' );
-// 	shell.exit( 1 );
-// }
-
-if ( shell.exec( `cp -rf build/ release-${ version }/build` ).code !== 0 ) {
+if ( shell.exec( `git checkout -b v${ version } ` ).code !== 0 ) {
 	shell.echo( 'Error' );
 	shell.exit( 1 );
 }
 
-if ( shell.exec( `cp media-manager.php readme.txt license.txt release-${ version }/` ).code !== 0 ) {
+if ( shell.exec( 'npm run build' ).code !== 0 ) {
 	shell.echo( 'Error' );
 	shell.exit( 1 );
 }
 
-if ( shell.exec( `zip -r release-${ version }/media-manager.zip release-${ version }` ).code !== 0 ) {
+if ( shell.exec( `cp bin/release/_gitignore .gitignore` ).code !== 0 ) {
 	shell.echo( 'Error' );
 	shell.exit( 1 );
 }
 
-if ( shell.exec( `git add release-${ version }/ -v` ).code !== 0 ) {
+if ( shell.exec( `git rm -rf --cache packages/ src/ bin/ Readme.md babel.config.js lerna.json package-lock.json package.json` ).code !== 0 ) {
+	shell.echo( 'Error' );
+	shell.exit( 1 );
+}
+
+if ( shell.exec( `zip -r media-manager-${ version }.zip build/ media-manager.php readme.txt license.txt` ).code !== 0 ) {
+	shell.echo( 'Error' );
+	shell.exit( 1 );
+}
+
+if ( shell.exec( `git add build/ .gitignore media-manager.php readme.txt license.txt media-manager-${ version }.zip -v -f` ).code !== 0 ) {
 	shell.echo( 'Error git adding files' );
 	shell.exit( 1 );
 }
@@ -48,4 +48,17 @@ if ( shell.exec( `git commit -m "media-manager: release-${ version }"` ).code !=
 	shell.exit( 1 );
 }
 
-// console.log( 'version: ', version );
+if ( shell.exec( `git tag v${ version }` ).code !== 0 ) {
+	shell.echo( 'Error' );
+	shell.exit( 1 );
+}
+
+if ( shell.exec( `git checkout -` ).code !== 0 ) {
+	shell.echo( 'Error' );
+	shell.exit( 1 );
+}
+
+if ( shell.exec( `git checkout -- build/` ).code !== 0 ) {
+	shell.echo( 'Error' );
+	shell.exit( 1 );
+}
